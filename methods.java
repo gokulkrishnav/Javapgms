d295747a83e3cdcdafdada24210ac7e6d06b1420
gokulkrishnav <gokulkrishnav342000@gmail.com>
2022-10-09 01:15:48 +0530

    first version

---
+++ methods.java
@@ -0,0 +1,18 @@
+public class methods {
+
+    public static void main(String[] args) {
+        String name = "gokul";
+        String surname = "krishna";
+        details(name ,surname);
+        System.out.println("my name is " +name);
+        System.out.println("my surname is " +surname);
+
+    }
+    static void details(String name,String surname){
+        name = "ravi" ;
+        surname = "raj" ;
+        System.out.println("my name is " +name);
+        System.out.println("my surname is " +surname);
+    }
+
+}

7d6a3012753bc8c586581d5440f108f92841b361
gokulkrishnav <72247730+gokulkrishnav@users.noreply.github.com>
2023-03-15 00:06:08 +0530

    Update PrimeNumOrNot.java

--- PrimeNumOrNot.java
+++ PrimeNumOrNot.java
@@ -3,21 +3,33 @@
 import java.util.Scanner;
 public class PrimeNumOrNot {
     public static void main(String []args){
-        int count =0;
-        Scanner sc =new Scanner(System.in);
+        Scanner sc = new Scanner(System.in)
         int n = sc.nextInt();
 
-        for(int i = 1;i <= n; i++){
-            if(n%i == 0) {
-                count++;
-            }
-        }
-        if(count == 2){
-            System.out.println("Prime number");
-        }
-        else{
-            System.out.println("Not a Prime Number");
-        }
+    // Holds the count of values
+    int count = 0;
+
+    // There is no prime number less than 2.
+    if (n <= 1) {
+      System.out.println("The number is not prime");
+      return;
+    }
+
+    // Do a for loop
+    for (int i = 1; i <= n / 2; i++) {
+      if (n % i == 0) {
+        count++;
+      }
+    }
+
+    // If the number of factors is greater than 1,
+    // the number is not prime.
+    if (count > 1) {
+        System.out.println("The number is not prime");
+    }
+    else {
+      System.out.println("The number is prime");
+    }
     }
 
 }
